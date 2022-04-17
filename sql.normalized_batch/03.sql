44c121dabe8b5bb80254052e912f298e403efc62
Julie <78115182+leafsphere@users.noreply.github.com>
2022-04-17 16:04:19 -0700

    Updated description and made code more concise

--- sql.normalized_batch/03.sql
+++ sql.normalized_batch/03.sql
@@ -1,17 +1,11 @@
 /*
- * Calculates the hashtags that are commonly used with the hashtag #coronavirus
+ * Calculates the languages that use the hashtag #coronavirus
  */
 SELECT
     lang,
-    count(*) as count
-FROM (
-    SELECT DISTINCT t2.id_tweets,lang
-    FROM tweet_tags t1
-    JOIN tweet_tags t2 ON t1.id_tweets = t2.id_tweets
-    JOIN tweets ON t2.id_tweets = tweets.id_tweets
-    WHERE t1.tag='#coronavirus'
-      AND t2.tag LIKE '#%'
-) t
+    count(DISTINCT id_tweets) as count
+FROM tweet_tags
+JOIN tweets USING (id_tweets)
+WHERE tag='#coronavirus'
 GROUP BY lang
 ORDER BY count DESC,lang;
-
